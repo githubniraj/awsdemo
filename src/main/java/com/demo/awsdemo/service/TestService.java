cc67bbb840251b02903e966405673f54c22b54ed
Ayusha POKHAREL <ayusha.poharel30@gmail.com>
2022-06-29 20:36:31 -0400

    Adding service class

---
+++ src/main/java/com/demo/awsdemo/service/TestService.java
@@ -0,0 +1,30 @@
+package com.demo.awsdemo.service;
+
+public class TestService {
+    public void testMethod() {
+
+        System.out.println("I am a test method");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
