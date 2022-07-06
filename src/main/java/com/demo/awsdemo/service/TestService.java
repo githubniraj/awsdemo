53cd8085c852c24782ff2c3916fbdacba193e7d6
bonlover <yungyalsen@gmail.com>
2022-07-05 23:52:58 -0400

    Adding Democontroller and TestService by completed PUT method controller API and business logic

--- src/main/java/com/demo/awsdemo/service/TestService.java
+++ src/main/java/com/demo/awsdemo/service/TestService.java
@@ -1,7 +1,16 @@
 package com.demo.awsdemo.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TestService {
     public void testMethod(){
         System.out.println("Modified method statement");
+    }
+
+    public ResponseEntity<String> updateTest(String hello) {
+
+        return  ResponseEntity.ok(String.format("%s , update successfully", hello));
     }
 }
