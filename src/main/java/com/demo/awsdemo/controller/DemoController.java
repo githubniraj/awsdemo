63b7e2692b7354d7d15dbf6917a48444e8314d69
Karma Sherpa <ksherpa@infinite.com>
2022-07-07 16:00:32 -0400

    modification file added

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.demo.awsdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class DemoController {
     @GetMapping(value = "/welcome")
     public String getHello(){
         return "Hello Niraj! Welcome to Brucewayne";
+    }
+
+    @PostMapping(value = "/user")
+    public String addUser() {
+        return "Hi its Karma";
     }
 }
