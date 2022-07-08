2984483911ec9a424b0998ba3199590ce4ee1e2d
Ramji <100006991+javarangers@users.noreply.github.com>
2022-07-07 20:41:48 -0400

    adding delete mapping

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.demo.awsdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping(value = "/welcome")
     public String getHello(){
+
         return "Hello Niraj! Welcome to Brucewayne";
+    }
+    @DeleteMapping(value="/user")
+    public String deleteUser(){
+
+        return "Delete user";
     }
 }
--- src/test/java/com/demo/awsdemo/service/TestService.java
+++ src/test/java/com/demo/awsdemo/service/TestService.java
@@ -1,6 +1,10 @@
 package com.demo.awsdemo.service;
 
+
+import org.springframework.stereotype.Component;
 public class TestService {
+
+
     public void testMethod(){
         System.out.println("this is my test method");
     }
