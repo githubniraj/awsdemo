e52a0d46a6dbb3fa2afad4e9f01362fd793a4853
ayush <ayusha.pokharel30@gmail.com>
2022-07-08 20:54:40 -0400

    updated controller file

--- src/main/java/com/demo/awsdemo/dao/TestDao.java
+++ src/main/java/com/demo/awsdemo/dao/TestDao.java
@@ -1,15 +1,34 @@
-package com.demo.awsdemo.dao;
 
+package com.demo.awsdemo.controller;
+
+import com.demo.awsdemo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Repository
-public class TestDao {
-    public ResponseEntity<String> updateTest(String msg) {
-        return ResponseEntity.ok(String.format("%s, You have update the message!" , msg));
-    }
-    public ResponseEntity<String> postTest(String str){
+@RestController
+public class DemoController {
 
-        return ResponseEntity.ok(String.format("Post this message", str));
+    @Autowired
+    TestService testService;
+    @GetMapping(value = "/welcome")
+    public String getHello(){
+        return "Hello Niraj! Welcome to Brucewayne";
     }
+
+    @PutMapping(value = "/update/{msg}")
+    public ResponseEntity<String> updateTest(@PathVariable(name = "msg") String msg){
+        return testService.updateTest(msg);
+    }
+
+    @PostMapping(value = "/user")
+    public ResponseEntity<String> postTest(){
+        return testService.postTest();
+    }
+
+
 }
