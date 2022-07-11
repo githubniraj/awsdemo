2e523feb8708b02bc10ded218e8e6504ee446901
ksherpa <ikarmasherpa@gmail.com>
2022-07-10 20:23:38 -0400

    new updated Dao, Service and Controller files added

--- src/main/java/com/demo/awsdemo/dao/TestDao.java
+++ src/main/java/com/demo/awsdemo/dao/TestDao.java
@@ -1,15 +1,12 @@
 package com.demo.awsdemo.dao;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TestDao {
-    public ResponseEntity<String> updateTest(String msg) {
-        return ResponseEntity.ok(String.format("%s, You have update the message!" , msg));
-    }
-    public ResponseEntity<String> postTest(String str){
 
-        return ResponseEntity.ok(String.format("Post this message", str));
+    public String postTest(){
+
+        return "Post successfully";
     }
 }
--- src/main/java/com/demo/awsdemo/service/TestService.java
+++ src/main/java/com/demo/awsdemo/service/TestService.java
@@ -2,25 +2,17 @@ package com.demo.awsdemo.service;
 
 import com.demo.awsdemo.dao.TestDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
+
     @Autowired
     TestDao testDao;
-    public void testMethod(){
-        System.out.println("Modified method statement");
+
+    public String postTest() {
+
+        return testDao.postTest();
     }
 
-    public ResponseEntity<String> postTest() {
-
-        return ResponseEntity.ok(String.format("%s , update successfully"));
-    }
-    public ResponseEntity<String> updateTest(String msg) {
-        return  testDao.updateTest(msg);
-    }
-    public ResponseEntity<String> deleteTest(){
-       return ResponseEntity.ok(String.format( " delete successfully"));
-    }
 }
