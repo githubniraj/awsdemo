a51c76cd7daf392c297223ea47bb3340d4ebc387
Sachina <shresthasachina3@gmail.com>
2022-08-05 20:02:31 -0500

    #18: initial commit

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.demo.awsdemo.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     @GetMapping(value = "/welcome")
-    public String getHello(){
+    public String getHello() {
         return "Hello Niraj! Welcome to Brucewayne";
+    }
+
+    public void testSachina() {
+        System.out.print("This is Sachina");
+
     }
 }
