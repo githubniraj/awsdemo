d616576949de1ecefb92ec88481974da0825d5e6
ayushapokharel <ayusha.pokharel30@gmail.com>
2022-08-05 20:53:56 -0400

    #16:Initial commit

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,13 +1,17 @@
 package com.demo.awsdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @GetMapping(value = "/welcome")
-    public String getHello(){
+    public String getHello() {
         return "Hello Niraj! Welcome to Brucewayne";
     }
+
+    public void testAyusha() {
+        System.out.println("This is Ayusha");
+    }
+
 }
