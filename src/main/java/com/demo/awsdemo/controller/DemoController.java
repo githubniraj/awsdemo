e4f895956bb5e5d682a1695cd7f00b18676d46ae
Saroj <mesaroj38@gmail.com>
2022-08-05 20:57:05 -0400

    Initial Commit

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,13 +1,16 @@
 package com.demo.awsdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @GetMapping(value = "/welcome")
-    public String getHello(){
+    public String getHello() {
         return "Hello Niraj! Welcome to Brucewayne";
+    }
+
+    public void testSaroj() {
+        System.out.println("Hello, This is Saroj");
     }
 }
