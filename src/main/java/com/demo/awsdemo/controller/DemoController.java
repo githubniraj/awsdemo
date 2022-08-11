3049e891794b098870ab985a498ec065b66a164a
Sachina <shresthasachina3@gmail.com>
2022-08-10 19:24:40 -0500

    #18 Adding delete method

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,29 +1,19 @@
 package com.demo.awsdemo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping(value = "/welcome")
-    public String getHello(){
-        return "Hello Niraj! Welcome to Brucewayne";
+    @GetMapping(value = "/get")
+    public String get() {
+        return " Test Get";
     }
 
-    public void testAjaya(){
-
-        System.out.println("Hello Ajay! Welcome to your ajay Branch");
-
-    }
-
-    public void testSaroj() {
-        System.out.println("Hello,This is Saroj");
-    }
-
-
-    public void testSachina() {
-        System.out.print("This is Sachina");
+    @DeleteMapping(value = "/delete")
+    public String delete() {
+        return "Test Post";
 
     }
 }
