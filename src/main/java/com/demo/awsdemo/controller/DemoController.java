7a234b02d339c638517c93da37ebf3f945674901
Saroj <mesaroj38@gmail.com>
2022-08-10 20:18:13 -0400

    #19:Put method added

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,23 +1,18 @@
 package com.demo.awsdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping(value = "/welcome")
-    public String getHello(){
-        return "Hello Niraj! Welcome to Brucewayne";
+    @GetMapping(value = "/get")
+    public String get() {
+        return "Test Get";
     }
 
-    public void testAjaya(){
-
-        System.out.println("Hello Ajay! Welcome to your ajay Branch");
-
-    }
-
-    public void testSaroj() {
-        System.out.println("Hello,This is Saroj");
+    @PutMapping(value = "/put")
+    public String put() {
+        return "Test Put";
     }
 }
