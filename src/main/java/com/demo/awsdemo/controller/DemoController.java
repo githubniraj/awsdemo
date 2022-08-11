0961f4351d4c661dd235973a393646c6caefbe22
ajpaudel <arspaudel@gmail.com>
2022-08-10 20:19:02 -0400

    changes the method in controller

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,42 +1,20 @@
 package com.demo.awsdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping(value = "/welcome")
-    public String getHello() {
-        return "Hello Niraj! Welcome to Brucewayne";
+    @GetMapping(value = "/get")
+    public String get() {
+        return "Test Get";
     }
 
 
-
-
-    public void testBibhasa() {
-        System.out.println("Hello!I am bibhasa");
-    }
-
-}
-    public void testAyusha() {
-        System.out.println("This is Ayusha");
-    }
-
-    public void testAjaya(){
-
-        System.out.println("Hello Ajay! Welcome to your ajay Branch");
-
-    }
-
-
-
-    public void testSaroj() {
-        System.out.println("Hello,This is Saroj");
-    }
-
-
-    public void testSachina() {
-        System.out.print("This is Sachina");
+    @PostMapping(value = "/post")
+    public String delete() {
+        return "Test Post";
 
     }
 }
