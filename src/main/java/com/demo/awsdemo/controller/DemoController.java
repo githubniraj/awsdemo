f5ce310e55478498dcfd1d82f82ca9c75c80eb92
Namshang <namshangprog@gmail.com>
2022-09-07 20:33:41 -0400

    #39: delete mapping

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,22 +1,20 @@
 package com.demo.awsdemo.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class DemoController {
-
-
-
-
     @GetMapping(value = "/get")
     public String get() {
+        return null;
+    }
+
+    @DeleteMapping(value = "/delete")
+    public String delete() {
         return null;
     }
 }
