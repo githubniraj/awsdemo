45492d398ee3fa377954d53822dbdd0bc5b951ad
mangalmaster <mangalmbt@gmail.com>
2022-09-07 20:29:52 -0400

    33: Adding post mapping

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,10 +1,7 @@
 package com.demo.awsdemo.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-
-
-
     @GetMapping(value = "/get")
     public String get() {
         return null;
     }
+
+    @PostMapping(value ="/post")
+    public String post(){
+        return null;
+    }
+
 }
