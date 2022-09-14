5e7ecc14e22de2ddae2c1814adfcc9a2f518dcdd
Dipshikha Tamang <dipshikhat99@gmail.com>
2022-09-13 21:07:31 -0500

    Initial commit

--- src/main/java/com/demo/awsdemo/service/DemoService.java
+++ src/main/java/com/demo/awsdemo/service/DemoService.java
@@ -1,34 +1,56 @@
 package com.demo.awsdemo.service;
 
+
 import com.demo.awsdemo.dao.DemoDao;
 import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.ResponseEntity;
 
 public class DemoService {
 
+
     DemoDao demoDao = new DemoDao();
 
-    public ResponseEntity<String> get() {
-       // HeaderToken headerTokern = HttpHeaders.readOnlyHttpHeaders();
-        //headerTokern.verify()//Verify if it is for Tesla or not
-        // if yes then proccess the request
-        // allow ther request
-        // request(latigute and longitue for Peureto rico)
-        // call the db
-        return demoDao.get();
-        // send response
+    public ResponseEntity<String> post(String input){
+        if(input == null || input.isEmpty())
+            return null;
+        if(input.equalsIgnoreCase("Dipsikha"))
+            throw new IllegalArgumentException("Exception");
+        if(input.equalsIgnoreCase("Namsang"))
+            return ResponseEntity.ok("Not valid");
+        return demoDao.post(input);
     }
 
+
+
+
+    public ResponseEntity<String> get() {
+        return demoDao.get();
+    }
     public ResponseEntity<String> put(){
         return demoDao.put();
     }
 
-    public ResponseEntity<String> post(){
-        return demoDao.post();
-    }
-
     public ResponseEntity<String> delete() {
         return demoDao.delete();
+
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("ok");
+    }
+    public ResponseEntity<String> put(){
+        return ResponseEntity.ok("ok");
+
+    }
+
+    public ResponseEntity<String> post(){
+        return ResponseEntity.ok("ok");
+    }
+
+
+    public ResponseEntity<String> delete() {
+
+        return ResponseEntity.ok("ok");
+
     }
 
 }
