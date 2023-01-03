f56ce858038f79df05f518f223b2457832ebe8e8
Namshang <namshangprog@gmail.com>
2023-01-02 20:59:28 -0500

    commit 1

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping(value = "/welcome")
     public String getHello(){
-        return "Hello Niraj! Welcome to Brucewayne";
+        return "Hello Namshang! Welcome to Thor";
     }
 }
