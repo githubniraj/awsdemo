7d03bfc770d7d91f5467552d2fb8c08df4670055
Namshang <namshangprog@gmail.com>
2023-01-03 21:20:51 -0500

    PutMapping commit

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.demo.awsdemo.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class DemoController {
     @GetMapping(value = "/welcome")
     public String getHello(){
         return "Hello Sunaina! Welcome to NC";
+    }
+
+    @PutMapping(value = "/welcome")
+    public String putHello(){
+        return "Hello Namshang! Welcome to NYC";
     }
 }
