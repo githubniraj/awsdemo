de8e06b04dfd5e3cfcbc1a4c37d3480c166972c6
alishasth <alyshashrestha@gmail.com>
2023-01-04 17:57:21 -0700

    delete mapping added

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.demo.awsdemo.controller;
 
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,12 @@ public class DemoController {
     public String getHello(){
         return "Hello Alisha! Welcome to Wakanda";
     }
+
+
+    @DeleteMapping(value = "/welcome")
+    public String delete(){
+        return "Hello Alisha! Welcome to DeleteMapping";
+    }
 }
+
+
