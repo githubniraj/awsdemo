ed1cfcf88be7517d7c2534c3b690000f93fecd92
unknown <s06081994@gmail.com>
2023-01-05 17:42:19 -0800

    first commit made

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,13 +1,13 @@
 package com.demo.awsdemo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoController {
-    @GetMapping(value = "/welcome")
-    public String getHello(){
-        return "Hello Niraj! Welcome to Brucewayne";
-    }
+
+
 }
+
+
+
