cf6f5c65994caf5b829495ea72857ab6121ca9b6
sonam lama <nepathya0608@gmail.com>
2023-01-05 19:07:44 -0800

    ok second commit.

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -9,28 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class DemoController {
 
-    @GetMapping(value = "/welcome")
-    public String getHello() {
-        return "Hello sonam! Welcome to Dallas";
-    }
 
-    //@PostMapping to feature branch
-    @PostMapping(value = "/welcome")
-    public String postHello(@RequestBody String message) {
-        return "Hey sonam, your message has been posted!!\n" + message;
-    }
-
-    @PutMapping(value = "/welcome")
-    public String putHello() {
-        return "Hello sonam! Welcome to NYC";
-    }
-    @DeleteMapping(value="/welcome")
-    public String deleteHello(){
-        return "Hello, your file has been deleted";
-    }
-    @PatchMapping(value = "/welcome")
-    public String patchHello() {
-        return "Hello sonam! Welcome to NYC";
-    }
 
 }
