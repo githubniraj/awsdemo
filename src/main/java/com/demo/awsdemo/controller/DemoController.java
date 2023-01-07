f1337a5e40b3ada0cfc4af4d900eabcbd662e7ec
mahima006 <tmahima.mht@gmail.com>
2023-01-06 21:06:25 -0500

    commit from mahima

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -9,6 +9,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class DemoController {
 
+    @GetMapping(value = "/welcome")
+    public String getHello() {
+        return "Hello Mahima! Welcome to Dallas";
+    }
 
+    //@PostMapping to feature branch
+    @PostMapping(value = "/welcome")
+    public String postHello(@RequestBody String message) {
+        return "Hey Mahima, your message has been posted!!\n" + message;
+    }
+
+    @PutMapping(value = "/welcome")
+    public String putHello() {
+        return "Hello Namshang! Welcome to NYC";
+    }
 
 }
