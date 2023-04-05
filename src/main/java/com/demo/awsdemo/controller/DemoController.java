45a9a692adb76362160e65a8cce010cd0825eee0
surendrakb <skbijukchhe2023@gmail.com>
2023-04-04 18:59:21 -0700

    4:Added PUT method

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.demo.awsdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class DemoController
     }
 
 
+    @PutMapping(value = "/put")
+    public String putStudents()
+    {
+        return "I'm PUT";
+    }
 
 }
