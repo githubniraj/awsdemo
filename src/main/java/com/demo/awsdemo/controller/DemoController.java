cdff52bf4fe01fdd466b9e182d842849ddb99f79
sanchay <snlgrglns@gmail.com>
2023-04-04 21:58:04 -0400

    3:delete method

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.demo.awsdemo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,9 @@ public class DemoController
         return "I'm Anil.";
     }
 
-
-
+    @DeleteMapping(value = "/delete")
+    public String deleteStudents()
+    {
+        return "I'm Delete.";
+    }
 }
