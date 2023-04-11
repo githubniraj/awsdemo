88c52eacb85b25367a88db1749d47cea9cc3e688
Shyam Nath <shyamnath@my.unt.edu>
2023-04-10 20:30:16 -0500

    4 : added post endpoint

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -1,9 +1,6 @@
 package com.demo.awsdemo.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoController
@@ -25,6 +22,12 @@ public class DemoController
     public String putStudents()
     {
         return "I'm PUT";
+    }
+
+    @PostMapping(value = "/post")
+    public String postStudents()
+    {
+        return "I'm Post";
     }
 
 }
