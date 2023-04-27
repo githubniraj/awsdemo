11221e07880c93302cc66d7ae4e715acddc8b84c
Anil Thapa <anilthapa@Anils-MBP-2.lan>
2023-04-26 20:48:32 -0500

    1: Unit testing; 2 test for getStudent() method

--- src/main/java/com/demo/awsdemo/controller/DemoController.java
+++ src/main/java/com/demo/awsdemo/controller/DemoController.java
@@ -2,7 +2,6 @@ package com.demo.awsdemo.controller;
 
 import com.demo.awsdemo.model.Student;
 import com.demo.awsdemo.service.DemoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,41 +9,36 @@ import java.util.Collection;
 import java.util.Optional;
 
 @RestController
-
-public class DemoController
-{
-    DemoService demoService=new DemoService();
+public class DemoController {
+    DemoService demoService = new DemoService();
 
     @GetMapping(value = "/get/{id}")
-    public ResponseEntity<Optional<Student>> getStudent(@PathVariable int id)
-    {
+    public ResponseEntity<Optional<Student>> getStudent(@PathVariable int id) {
         Optional<Student> student = demoService.getStudent(id);
         return ResponseEntity.ok().body(student);
     }
 
     @GetMapping(value = "/getall")
-    public ResponseEntity<Collection<Student>>  getStudents()
-    {
+    public ResponseEntity<Collection<Student>> getStudents() {
         Collection<Student> students = demoService.getAllStudent();
         return ResponseEntity.ok().body(students);
     }
 
     @PostMapping(value = "/post")
-    public ResponseEntity<Integer> postStudent(Student student)
-    {
+    public ResponseEntity<Integer> postStudent(Student student) {
         return ResponseEntity.ok().body(demoService.postStudent(student));
     }
 
     @PutMapping(value = "/put")
-    public ResponseEntity<Integer> putStudent(Student student)
-    {
+    public ResponseEntity<Integer> putStudent(Student student) {
         return ResponseEntity.ok().body(demoService.putStudent(student));
     }
 
     @DeleteMapping(value = "/delete/{id}")
-    public ResponseEntity<Integer> deleteStudent(@PathVariable int id)
-    {
+    public ResponseEntity<Integer> deleteStudent(@PathVariable int id) {
         return ResponseEntity.ok().body(demoService.deleteStudent(id));
 
     }
+
+
 }
---
+++ src/test/java/com/demo/awsdemo/unit/DemoServiceTest.java
@@ -0,0 +1,64 @@
+package com.demo.awsdemo.unit;
+
+import com.demo.awsdemo.dao.DemoDAO;
+import com.demo.awsdemo.model.Student;
+import com.demo.awsdemo.service.DemoService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class DemoServiceTest {
+
+    @Mock
+    private DemoDAO demoDAO;
+
+    @InjectMocks
+    private DemoService demoService;
+
+    @BeforeAll
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    public void testGetStudentHappyPath(){
+        Mockito.when(demoDAO.get(Mockito.anyInt())).thenReturn(Optional.of(new Student()));
+        Optional<Student> student = demoService.getStudent(1);
+        assertTrue(student.get()!=null);
+        assertEquals(null, student.get().getName());
+        Mockito.verify(demoDAO, Mockito.times(1)).get(Mockito.anyInt());
+    }
+
+
+    @Test
+    public void testGetStudentEmptyHappyPath(){
+        Mockito.when(demoDAO.get(Mockito.anyInt())).thenReturn(Optional.empty());
+        Optional<Student> student = demoService.getStudent(1);
+        assertTrue(student.get()!=null);
+        assertEquals(null, student.get().getName());
+        Mockito.verify(demoDAO, Mockito.times(1)).get(Mockito.anyInt());
+    }
+
+    @Test
+    public void testGetStudentExceptionPath(){
+        Mockito.when(demoDAO.get(Mockito.anyInt())).thenThrow(new IllegalArgumentException("Illegal arguments"));
+        Optional<Student> student = demoService.getStudent(1);
+        assertTrue(!student.isPresent());
+        Mockito.verify(demoDAO,Mockito.times(1)).get(Mockito.anyInt());
+    }
+}
