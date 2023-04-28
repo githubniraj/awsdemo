7704755d6b186da0683d7bcbfd48a2d6cf144ab6
Anil Thapa <anilthapa@Anils-MBP-2.lan>
2023-04-27 21:00:45 -0500

    1: Unit testing; test cases for delete, post and put studet

--- src/test/java/com/demo/awsdemo/unit/DemoServiceTest.java
+++ src/test/java/com/demo/awsdemo/unit/DemoServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,29 +37,71 @@ public class DemoServiceTest {
 
 
     @Test
-    public void testGetStudentHappyPath(){
+    public void testGetStudentHappyPath() {
         Mockito.when(demoDAO.get(Mockito.anyInt())).thenReturn(Optional.of(new Student()));
         Optional<Student> student = demoService.getStudent(1);
         assertTrue(student.get()!=null);
         assertEquals(null, student.get().getName());
         Mockito.verify(demoDAO, Mockito.times(1)).get(Mockito.anyInt());
     }
-
-
     @Test
-    public void testGetStudentEmptyHappyPath(){
-        Mockito.when(demoDAO.get(Mockito.anyInt())).thenReturn(Optional.empty());
+    public void testGetStudentExceptionPath() {
+        Mockito.when(demoDAO.get(Mockito.anyInt())).thenThrow(new IllegalArgumentException("Illegal arguments"));
         Optional<Student> student = demoService.getStudent(1);
-        assertTrue(student.get()!=null);
-        assertEquals(null, student.get().getName());
+        assertTrue(!student.isPresent());
         Mockito.verify(demoDAO, Mockito.times(1)).get(Mockito.anyInt());
     }
 
     @Test
-    public void testGetStudentExceptionPath(){
-        Mockito.when(demoDAO.get(Mockito.anyInt())).thenThrow(new IllegalArgumentException("Illegal arguments"));
-        Optional<Student> student = demoService.getStudent(1);
-        assertTrue(!student.isPresent());
-        Mockito.verify(demoDAO,Mockito.times(1)).get(Mockito.anyInt());
+    public void testGetAllStudentHappyPath(){
+        Mockito.when(demoDAO.getAll()).thenReturn(Collections.emptyList());
+        Collection<Student> studentCollection = demoService.getAllStudent();
+        assertTrue(studentCollection !=null);
+        assertEquals(0, studentCollection.size());
+        Mockito.verify(demoDAO, Mockito.times(1)).getAll();
     }
+
+    @Test
+    public void testDeleteStudentHappyPath(){
+        Mockito.when(demoDAO.delete(Mockito.anyInt())).thenReturn(0);
+        int num = demoService.deleteStudent(1);
+        assertTrue(num ==0);
+        Mockito.verify(demoDAO, Mockito.times(1)).delete(Mockito.anyInt());
+    }
+
+
+    @Test
+    public void testPostStudentHappyPath(){
+        Mockito.when(demoDAO.post(Mockito.any())).thenReturn(1);
+        int num = demoService.postStudent(new Student(9,"Thapa"));
+        assertEquals(1, num);
+        Mockito.verify(demoDAO, Mockito.times(1)).post(Mockito.any());
+    }
+
+    @Test
+    public void testPutStudentHappyPath(){
+        Mockito.when(demoDAO.put(Mockito.any())).thenReturn(1);
+        int num = demoService.putStudent(new Student(100,"Sushant"));
+        assertEquals(num, 1);
+        Mockito.verify(demoDAO, Mockito.times(1)).put(Mockito.any());
+    }
+
 }
+
+//    @Test
+//    public void testGetStudentEmptyHappyPath() {
+//        Mockito.when(demoDAO.get(Mockito.anyInt())).thenReturn(Optional.empty());
+//        Optional<Student> student = demoService.getStudent(1);
+//        assertTrue(student.get() != null);
+//        assertEquals(null, student.get().getName());
+//        Mockito.verify(demoDAO, Mockito.times(1)).get(Mockito.anyInt());
+//    }
+
+//    @Test
+//    public void testGetAllStudentExceptionPath(){
+//        Mockito.when(demoDAO.getAll()).thenThrow(new NullPointerException("No object found"));
+//        Collection<Student> studentCollection = demoService.getAllStudent();
+//        assertTrue(!studentCollection.isEmpty());
+//        Mockito.verify(demoDAO, Mockito.times(1)).getAll();
+//    }
+
